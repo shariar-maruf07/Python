fb6d846d7f9bd3f02a82d8731b2b6e6f4fa7b224
shariar-maruf07 <shariarhabib11223@gmail.com>
2026-08-10 19:37:33 +0600

    new

--- 04 strings.py
+++ 04 strings.py
@@ -3,6 +3,9 @@
 name= "Shariar Maruf"
 grade= 'A'
 
+#string length ber krte
+print("String length", len(name))
+
 #upper case and lower case
 print("uppercase: " ,name.upper()) #upper case e convert korbe
 print("lowercase: " ,grade.lower()) #lower case e convert korbe
@@ -21,4 +24,22 @@ print("Check 'khan' is in name: ", "khan" in name)
 
 print("Now the main string: ", name)
 
+#concatenate
+word1="i love"
+word2="you"
+print(word1+" "+word2)
+
+#prottek charecter k print korabo
+for x in word2:
+    print(x)
+    
+#slicing
+print(name[2:8]) #substring print koraite hole ___string___[starting index(def=0):ending index(def=str length)]
+
+#string formating
+a=5
+b=10
+print(f"the sum of {a} and {b} is {a+b}") #ebhabe f dia string er modde likha jabe alada krte hbe na
+
 #string er khetre amra string e joto kisoi na kori seta main string ke change korbe na tobe seta ekta noton string banabe. karon string immutable.
+
